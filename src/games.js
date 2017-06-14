6effa80de8f2f66318d8be103248d089d56cab62
Koen Handekyn <koen.handekyn@up-nxt.com>
2017-06-14 08:44:21 +0200

    random game names with npm package instead of service

--- src/games.js
+++ src/games.js
@@ -1,13 +1,12 @@
-// import {gun} from 'gun';
 import {GunDb} from 'gundb';
 import {inject} from 'aurelia-framework';
-// import {lodash} from 'lodash';
+// import {default as word} from 'words'; // alternative for the below let randomWords = require('words');    
 
 @inject(GunDb)
 export class Games {  
-  constructor(gunDb)  {
-    this.gamesDb = gunDb.db.get('games/koen');
+  constructor(gunDb)  {    
     this.games = new Map();    
+    this.gamesDb = gunDb.db.get('games/koen');
     this.gamesDb.map().on((game, id) => {      
       if (game) { 
         this.games.set(id,game);
@@ -26,10 +25,16 @@ export class Games {
   }
 
   newGame() { 
-    console.log('fetching random name for game');
-    fetch('http://setgetgo.com/randomword/get.php')
-      .then((response) => { return response.text(); })
-      .then((name) => { console.log('response', name); return this.gamesDb.set( {name: name} ); });
+    console.log("new game");
+    let randomWords = require('words');    
+    let name = randomWords();
+    this.gamesDb.set( {name: name} );
+    // alternatively use a service for fetching a random word
+    // it's not working however due to mixed http and https traffic
+    // console.log('fetching random name for game');
+    // fetch('http://setgetgo.com/randomword/get.php')
+    //   .then((response) => { return response.text(); })
+    //   .then((name) => { console.log('response', name); return this.gamesDb.set( {name: name} ); });
   }
 }
 
